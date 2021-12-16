0ba9a7153267311be4046396d9710759f17597eb
EGuyett <e.guyett.93@gmail.com>
2021-12-16 14:45:59 -0500

    Finished Featured recipes, search bar with icons, about us, follow. newly added, and featured categories tab component

--- Family Cookbook/Assets/JavaScript/cookBookhome.js
+++ Family Cookbook/Assets/JavaScript/cookBookhome.js
@@ -1,4 +1,74 @@
 "use strict";
+// Selectors:
+const tabs = document.querySelectorAll(".featured_categories_tab ");
+const tabsContainer = document.querySelector(
+  ".featured_categories_tab_container"
+);
+const tabsContent = document.querySelectorAll(".featured_categories_content");
 
-///////////////////////
-//Selectors:
+////////////////////////
+// Reveal sections:
+const allSections = document.querySelectorAll(".section");
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  entry.target.classList.remove("section--hidden");
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add("section--hidden");
+});
+
+// Lazy loading images:
+let options = {
+  root: document.querySelector(".root"),
+  rootMargin: "0px, 0px, 100px, 0px",
+};
+
+let images = [...document.querySelectorAll(".target_images")];
+const callback = (entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.src = entry.target.dataset.src;
+    }
+  });
+};
+
+let observer = new IntersectionObserver(callback);
+
+images.forEach((image) => {
+  observer.observe(image);
+});
+
+/////////////////////////////
+// Tabbed component
+tabsContainer.addEventListener("click", function (e) {
+  const clicked = e.target.closest(".featured_categories_tab");
+
+  // Guard clause
+  if (!clicked) return;
+
+  // Remove active classes
+  tabs.forEach((t) => t.classList.remove("featured_categories_tab--active"));
+  tabsContent.forEach((c) =>
+    c.classList.remove("featured_categories_content--active")
+  );
+
+  // Activate tab
+  clicked.classList.add("featured_categories_tab--active");
+
+  // Activate content area
+  document
+    .querySelector(`.featured_categories_content--${clicked.dataset.tab}`)
+    .classList.add("featured_categories_content--active");
+});
